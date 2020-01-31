47a1638bf01b70196e82755d655a4a8b45cb9fc5
oexlye08 <os.alimie@gmail.com>
2020-01-31 11:37:54 +0700

    added link api

--- app/src/main/java/id/web/crudretrofit/api/ApiClient.java
+++ app/src/main/java/id/web/crudretrofit/api/ApiClient.java
@@ -3,6 +3,15 @@ package id.web.crudretrofit.api;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+
+/** link download sql database https://pastebin.com/GyCpfUkd*/
+/** link download api connect https://pastebin.com/448ijkXx*/
+/** link download api delete https://pastebin.com/i8Aed7jf*/
+/** link download api notes https://pastebin.com/DxrDTQPm*/
+/** link download api save https://pastebin.com/Ykinbfcr*/
+/** link download api update https://pastebin.com/LVXG4Lu8*/
+
+
 public class ApiClient {
 
 //    private static final String BASE_URL = "http://206.189.6.93:8087/oki/";
